e44ab15c126e762ffb4e26c094d77a83dad900ad
InFog <junior@casoft.info>
2012-05-07 23:54:32 -0300

    Adicionados templates básicos
    Adicionei a base dos templates e as chamadas.
    Pretendo melhorar um pouco para evitar repetições.

--- bottlewiki.py
+++ bottlewiki.py
@@ -11,17 +11,28 @@
 
 from os import path
 
-from lib.bottle import route, get, post, run
+from lib.bottle import route, get, post, run, template
 from lib.markdown2 import markdown
+
+dados_view = {
+    'site_url' : 'http://localhost:8080/'
+}
+
 
 @get('/')
 @get('/wiki')
 @get('/wiki/<pagina>')
 def wiki(pagina="home"):
+    dados_view['pagina'] = pagina
     pagina = './wiki/' + pagina + '.md'
     if (path.exists(pagina)):
-        conteudo = open(pagina, 'r')
-        return markdown(conteudo.read())
+        arquivo = open(pagina, 'r')
+        conteudo = markdown(arquivo.read())
+    else:
+        conteudo = template('templates/form.html', dados_view)
+    conteudo = template('templates/cabecalho.html', dados_view) + conteudo
+    conteudo += template('templates/rodape.html')
+    return conteudo
 
 if __name__ == '__main__':
     run(host='localhost', port=8080)
